ceb924bcd0f93a4670e838687e363c397c9941ff
Aamir Nazir Kuchay <aamir1kuchay@gmail.com>
2024-07-19 12:59:48 +0530

    product size crud

--- src/main/java/com/ecommerce/repository/ProductSizeRepository.java
+++ src/main/java/com/ecommerce/repository/ProductSizeRepository.java
@@ -4,6 +4,9 @@ import com.ecommerce.entity.ProductSize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductSizeRepository extends JpaRepository<ProductSize,Long> {
+    List<ProductSize> findByProductId(Long productId);
 }
