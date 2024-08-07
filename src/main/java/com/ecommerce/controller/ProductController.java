e356cabe3beac80deafeb9f6abf225251a9948c2
Aamir Kuchay <aamir1kuchay@gmail.com>
2024-08-07 15:50:25 +0530

    product

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -2,30 +2,24 @@ package com.ecommerce.controller;
 
 import com.ecommerce.dto.ProductDto;
 import com.ecommerce.dto.ProductResponseDto;
-import com.ecommerce.entity.Product;
 import com.ecommerce.service.ProductService;
-import com.ecommerce.service.impl.FileStorageService;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/products")
 public class ProductController {
+
+
     private final ProductService productService;
 
 
